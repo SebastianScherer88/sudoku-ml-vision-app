4661c2adabf87c8fc841bc611a729e0baebf60e2
bettmensch88 <sebastian.scherer@yahoo.de>
2020-05-21 22:51:58 +0100

    added the python restapi that solves sudokus; added testing of restapi to lp_solver script; also updated indexing for row and col to range 0-8 as integers, and values from 1-9 as intergers

--- lp_solver.py
+++ lp_solver.py
@@ -2,12 +2,14 @@
 from pulp import *
 
 # A list of strings from "1" to "9" is created
-Sequence = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+#Sequence = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+Index = [0,1,2,3,4,5,6,7,8]
+Sequence = [1,2,3,4,5,6,7,8,9]
 
 # The Vals, Rows and Cols sequences all follow this form
 Vals = Sequence
-Rows = Sequence
-Cols = Sequence
+Rows = Index
+Cols = Index
 
 # The boxes list is created, with the row and column index of each square in each box
 Boxes =[]
@@ -41,33 +43,63 @@ for v in Vals:
         prob += lpSum([choices[v][r][c] for (r,c) in b]) == 1,""
                         
 # The starting numbers are entered as constraints. Note: choices is indexed by value, row, column.                
-prob += choices["2"]["1"]["2"] == 1,""
-prob += choices["9"]["1"]["5"] == 1,""
-prob += choices["5"]["1"]["8"] == 1,""
-prob += choices["3"]["1"]["9"] == 1,""
-prob += choices["9"]["2"]["1"] == 1,""
-prob += choices["3"]["3"]["4"] == 1,""
-prob += choices["5"]["3"]["5"] == 1,""
-prob += choices["4"]["3"]["6"] == 1,""
-prob += choices["7"]["4"]["3"] == 1,""
-prob += choices["6"]["4"]["6"] == 1,""
-prob += choices["8"]["4"]["7"] == 1,""
-prob += choices["9"]["4"]["8"] == 1,""
-prob += choices["1"]["5"]["1"] == 1,""
-prob += choices["5"]["5"]["3"] == 1,""
-prob += choices["2"]["5"]["7"] == 1,""
-prob += choices["9"]["6"]["3"] == 1,""
-prob += choices["5"]["6"]["4"] == 1,""
-prob += choices["2"]["6"]["6"] == 1,""
-prob += choices["4"]["6"]["9"] == 1,""
-prob += choices["8"]["7"]["4"] == 1,""
-prob += choices["2"]["7"]["5"] == 1,""
-prob += choices["6"]["7"]["9"] == 1,""
-prob += choices["8"]["8"]["1"] == 1,""
-prob += choices["7"]["8"]["4"] == 1,""
-prob += choices["7"]["9"]["1"] == 1,""
-prob += choices["5"]["9"]["6"] == 1,""
-prob += choices["4"]["9"]["7"] == 1,""
+# =============================================================================
+# prob += choices["2"]["1"]["2"] == 1,""
+# prob += choices["9"]["1"]["5"] == 1,""
+# prob += choices["5"]["1"]["8"] == 1,""
+# prob += choices["3"]["1"]["9"] == 1,""
+# prob += choices["9"]["2"]["1"] == 1,""
+# prob += choices["3"]["3"]["4"] == 1,""
+# prob += choices["5"]["3"]["5"] == 1,""
+# prob += choices["4"]["3"]["6"] == 1,""
+# prob += choices["7"]["4"]["3"] == 1,""
+# prob += choices["6"]["4"]["6"] == 1,""
+# prob += choices["8"]["4"]["7"] == 1,""
+# prob += choices["9"]["4"]["8"] == 1,""
+# prob += choices["1"]["5"]["1"] == 1,""
+# prob += choices["5"]["5"]["3"] == 1,""
+# prob += choices["2"]["5"]["7"] == 1,""
+# prob += choices["9"]["6"]["3"] == 1,""
+# prob += choices["5"]["6"]["4"] == 1,""
+# prob += choices["2"]["6"]["6"] == 1,""
+# prob += choices["4"]["6"]["9"] == 1,""
+# prob += choices["8"]["7"]["4"] == 1,""
+# prob += choices["2"]["7"]["5"] == 1,""
+# prob += choices["6"]["7"]["9"] == 1,""
+# prob += choices["8"]["8"]["1"] == 1,""
+# prob += choices["7"]["8"]["4"] == 1,""
+# prob += choices["7"]["9"]["1"] == 1,""
+# prob += choices["5"]["9"]["6"] == 1,""
+# prob += choices["4"]["9"]["7"] == 1,""
+# =============================================================================
+
+prob += choices[1][0][2] == 1,""
+prob += choices[9][0][4] == 1,""
+prob += choices[5][0][7] == 1,""
+prob += choices[3][0][8] == 1,""
+prob += choices[9][1][0] == 1,""
+prob += choices[3][2][3] == 1,""
+prob += choices[5][2][4] == 1,""
+prob += choices[4][2][5] == 1,""
+prob += choices[7][3][2] == 1,""
+prob += choices[6][3][5] == 1,""
+prob += choices[8][3][6] == 1,""
+prob += choices[9][3][7] == 1,""
+prob += choices[1][4][0] == 1,""
+prob += choices[5][4][2] == 1,""
+prob += choices[2][4][6] == 1,""
+prob += choices[9][5][2] == 1,""
+prob += choices[5][5][3] == 1,""
+prob += choices[2][5][5] == 1,""
+prob += choices[4][5][8] == 1,""
+prob += choices[8][6][3] == 1,""
+prob += choices[2][6][4] == 1,""
+prob += choices[6][6][8] == 1,""
+prob += choices[8][7][0] == 1,""
+prob += choices[7][7][3] == 1,""
+prob += choices[7][8][0] == 1,""
+prob += choices[5][8][5] == 1,""
+prob += choices[4][8][6] == 1,""
 
 # The problem data is written to an .lp file
 prob.writeLP("Sudoku.lp")
@@ -83,21 +115,102 @@ sudokuout = open('sudokuout.txt','w')
 
 # The solution is written to the sudokuout.txt file 
 for r in Rows:
-    if r == "1" or r == "4" or r == "7":
+    #if r == "1" or r == "4" or r == "7":
+    if r == 0 or r == 3 or r == 6:
                     sudokuout.write("+-------+-------+-------+\n")
     for c in Cols:
         for v in Vals:
             if value(choices[v][r][c])==1:
                                
-                if c == "1" or c == "4" or c =="7":
+                #if c == "1" or c == "4" or c =="7":
+                if c == 0 or c == 3 or c ==6:
                     sudokuout.write("| ")
                     
-                sudokuout.write(v + " ")
+                sudokuout.write(str(v) + " ")
                 
-                if c == "9":
+                #if c == "9":
+                if c == 8:
                     sudokuout.write("|\n")
 sudokuout.write("+-------+-------+-------+")                    
 sudokuout.close()
 
 # The location of the solution is give to the user
 print("Solution Written to sudokuout.txt")
+
+# --- test rest api
+from sudoku_solver_api import InitialValueConstraint,InitialValueConstraints
+import requests
+
+constraints = [
+    InitialValueConstraint(value=1,row_index=0,column_index=2), # prob += choices[1][0][2] == 1,""
+    InitialValueConstraint(value=9,row_index=0,column_index=4), # prob += choices[9][0][4] == 1,""
+    InitialValueConstraint(value=5,row_index=0,column_index=7), # prob += choices[5][0][7] == 1,""
+    InitialValueConstraint(value=3,row_index=0,column_index=8), # prob += choices[3][0][8] == 1,""
+    InitialValueConstraint(value=9,row_index=1,column_index=0), # prob += choices[9][1][0] == 1,""
+    InitialValueConstraint(value=3,row_index=2,column_index=3), # prob += choices[3][2][3] == 1,""
+    InitialValueConstraint(value=5,row_index=2,column_index=4), # prob += choices[5][2][4] == 1,""
+    InitialValueConstraint(value=4,row_index=2,column_index=5), # prob += choices[4][2][5] == 1,""
+    InitialValueConstraint(value=7,row_index=3,column_index=2), # prob += choices[7][3][2] == 1,""
+    InitialValueConstraint(value=6,row_index=3,column_index=5), # prob += choices[6][3][5] == 1,""
+    InitialValueConstraint(value=8,row_index=3,column_index=6), # prob += choices[8][3][6] == 1,""
+    InitialValueConstraint(value=9,row_index=3,column_index=7), # prob += choices[9][3][7] == 1,""
+    InitialValueConstraint(value=1,row_index=4,column_index=0), # prob += choices[1][4][0] == 1,""
+    InitialValueConstraint(value=5,row_index=4,column_index=2), # prob += choices[5][4][2] == 1,""
+    InitialValueConstraint(value=2,row_index=4,column_index=6), # prob += choices[2][4][6] == 1,""
+    InitialValueConstraint(value=9,row_index=5,column_index=2), # prob += choices[9][5][2] == 1,""
+    InitialValueConstraint(value=5,row_index=5,column_index=3), # prob += choices[5][5][3] == 1,""
+    InitialValueConstraint(value=2,row_index=5,column_index=5), # prob += choices[2][5][5] == 1,""
+    InitialValueConstraint(value=4,row_index=5,column_index=8), # prob += choices[4][5][8] == 1,""
+    InitialValueConstraint(value=8,row_index=6,column_index=3), # prob += choices[8][6][3] == 1,""
+    InitialValueConstraint(value=2,row_index=6,column_index=4), # prob += choices[2][6][4] == 1,""
+    InitialValueConstraint(value=6,row_index=6,column_index=8), # prob += choices[6][6][8] == 1,""
+    InitialValueConstraint(value=8,row_index=7,column_index=0), # prob += choices[8][7][0] == 1,""
+    InitialValueConstraint(value=7,row_index=7,column_index=3), # prob += choices[7][7][3] == 1,""
+    InitialValueConstraint(value=7,row_index=8,column_index=0), # prob += choices[7][8][0] == 1,""
+    InitialValueConstraint(value=5,row_index=8,column_index=5), # prob += choices[5][8][5] == 1,""
+    InitialValueConstraint(value=4,row_index=8,column_index=6)] # prob += choices[4][8][6] == 1]
+
+initial_grid = InitialValueConstraints(initial_values = constraints).json()
+
+sudoku_app_url = 'http://127.0.0.1:8000'
+
+r1 = requests.get(sudoku_app_url + '/')
+print(r1)
+
+r2 = requests.post(sudoku_app_url + '/solve',data = initial_grid)
+print(r2.text)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
---
+++ sudoku_solver_api.py
@@ -0,0 +1,163 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 21 20:38:07 2020
+
+The sudoku solving restapi thats called from the rshiny app
+
+@author: bettmensch
+"""
+
+from fastapi import FastAPI
+from pydantic import BaseModel, validator, ValidationError
+from typing import List
+
+from pulp import LpProblem, LpMinimize, LpVariable, lpSum, LpInteger, value
+
+app = FastAPI()
+
+# --- body and response classes
+class InitialValueConstraint(BaseModel):
+    
+    row_index: int
+    column_index: int
+    value: int
+    
+    @validator('row_index')
+    def check_row_range(cls, v):
+        if v not in range(9):
+            raise ValueError('Row index must be integer in [1,9]')
+        return v
+    
+    @validator('column_index')
+    def check_column_range(cls, v):
+        if v not in range(9):
+            raise ValueError('Column index must be integer in [1,9]')
+        return v
+    
+    @validator('value')
+    def check_initial_value_range(cls, v):
+        if v not in range(1,10):
+            raise ValueError('Initital value must be integer in [1,9]')
+        return v
+    
+class InitialValueConstraints(BaseModel):
+    
+    initial_values: List[InitialValueConstraint]
+    
+class SudokuSolution(BaseModel):
+    
+    solved: int
+    solution: List[List[int]] # outer lists are rows, inner lists are column indices
+    
+    @validator('solved')
+    def check_solution_status(cls,v):
+        if v not in (-1,1):
+            raise ValueError('Sudoku solution status must be either 1 for solved, or -1 for failed.')
+            
+        return v
+            
+    @validator('solution')
+    def check_solved_grid(cls,v):
+               
+        if len(v) != 9:
+            raise ValueError('Solution grid must have 9 rows.')
+            
+        for r in v:
+            if len(r) != 9:
+                raise ValueError('Solution grid must have 9 columns.')
+                
+            for d in r:
+                if d not in (1,2,3,4,5,6,7,8,9):
+                    raise ValueError('Solution must contain integers in [1,9] only.')
+                    
+        return v
+                    
+# =============================================================================
+# 
+# try:
+#     constraint_1 = InitialValueConstraint(row_index = 2, column_index = 3, value = 6)
+#     constraint_2 = InitialValueConstraint(row_index = 1, column_index = 7, value = 1)
+#     constraint_3 = InitialValueConstraint(row_index = 5, column_index = 8, value = 3)
+#     constraints = InitialValueConstraints(initial_values = [constraint_1, constraint_2, constraint_3])
+#     for i in constraints.initial_values:
+#         print(i.row_index)
+#         
+# except ValidationError as e:
+#     print(e)
+# 
+# try:
+#     solution = SudokuSolution(solved = 1,
+#                               solution = [[1,2,3,4,5,6,7,8,9] for i in range(9)])
+#     
+#     print(solution)
+# except ValidationError as e:
+#     print(e)
+# =============================================================================
+
+@app.get('/')
+def root():
+    return {'message':'Hello World!'}
+
+@app.post('/solve', response_model = SudokuSolution)
+def solve_sudoku(initial_value_constraints: InitialValueConstraints = None):
+    
+    # --- set up Sudoku problem
+    # The Vals, Rows and Cols sequences all follow this form
+    Vals = [1,2,3,4,5,6,7,8,9]
+    Rows = [0,1,2,3,4,5,6,7,8]
+    Cols = [0,1,2,3,4,5,6,7,8]
+    
+    # The boxes list is created, with the row and column index of each square in each box
+    Boxes =[]
+    for i in range(3):
+        for j in range(3):
+            Boxes += [[(Rows[3*i+k],Cols[3*j+l]) for k in range(3) for l in range(3)]]
+    
+    # The prob variable is created to contain the problem data        
+    prob = LpProblem("Sudoku Problem",LpMinimize)
+    
+    # The problem variables are created
+    choices = LpVariable.dicts("Choice",(Vals,Rows,Cols),0,1,LpInteger)
+    
+    # The arbitrary objective function is added
+    prob += 0, "Arbitrary Objective Function"
+    
+    # A constraint ensuring that only one value can be in each square is created
+    for r in Rows:
+        for c in Cols:
+            prob += lpSum([choices[v][r][c] for v in Vals]) == 1, ""
+    
+    # The row, column and box constraints are added for each value
+    for v in Vals:
+        for r in Rows:
+            prob += lpSum([choices[v][r][c] for c in Cols]) == 1,""
+            
+        for c in Cols:
+            prob += lpSum([choices[v][r][c] for r in Rows]) == 1,""
+    
+        for b in Boxes:
+            prob += lpSum([choices[v][r][c] for (r,c) in b]) == 1,""
+
+    # add initial values if needed
+    if initial_value_constraints != None:
+        for initial_value_constraint in initial_value_constraints.initial_values:
+            prob += choices[initial_value_constraint.value][initial_value_constraint.row_index][initial_value_constraint.column_index] == 1,""
+            
+    # --- solve sudoku with linear programing
+    solution_status = prob.solve()
+
+    if solution_status == -1:
+
+        return SudokuSolution(solved = solution_status)
+    elif solution_status == 1:
+
+        solved_grid = [[None]*9 for i in range(9)]
+        
+        for row in Rows:
+            for column in Cols:
+                for val in Vals:
+                    if value(choices[val][row][column]) == 1:
+                        solved_grid[row][column] = val
+
+        return SudokuSolution(solved = solution_status,
+                              solution = solved_grid)
