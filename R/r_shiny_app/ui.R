c8ff082c4a34b1ae370544ff7147b225b15ed8fd
bettmensch88 <sebastian.scherer@yahoo.de>
2020-05-25 16:36:29 +0100

    output now more square shaped

--- R/r_shiny_app/ui.R
+++ R/r_shiny_app/ui.R
@@ -31,7 +31,7 @@ ui <- navbarPage(
                              class = 'character')),
           column(width = 1,
                  h3('')),
-          column(width = 5,
+          column(width = 2,
                  h3('Solution'),
                  dataTableOutput('solution_grid'),
                  textOutput('solution_message'))
